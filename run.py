6e5e7d2d50a50d7c57d12ba9184707661bc696a6
niclastanskanen <niclastanskanen@hotmail.com>
2022-09-22 08:52:16 +0000

    Add calculate and return number sandwiches to make next market

--- run.py
+++ run.py
@@ -118,6 +118,14 @@ def calculate_stock_data(data):
 
     return new_stock_data
 
+def get_stock_values(data):
+    """
+    Calculate and return number of sandwiches to make next market
+    """
+    headings = SHEET.worksheet("stock").row_values(1)
+    return {heading:value for heading, value in zip(headings, data)}
+
+
 
 def main():
     """
@@ -131,6 +139,8 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    stock_values = get_stock_values(stock_data)
+    print(stock_values)
 
 
 print("Welcome to Love Sandwiches Data Automation")
